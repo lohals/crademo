23c44c8e32d773607ceb739fa24e29c6aba23ea3
Rasmus Lohals <lohals@gmail.com>
2019-07-18 21:50:28 +0200

    Not working

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/button";
+import { create, Jss } from "jss";
+import { jssPreset, StylesProvider } from "@material-ui/styles";
+
+const jss: Jss = create({
+  ...jssPreset(),
+  // define a custom insertion point that JSS will look for when injecting the styles into the DOM.
+  insertionPoint: "jss-insertion-point"
+});
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Button>Hello</Button>
-    </div>
+    <StylesProvider jss={jss}>
+      <div className="App">
+        <Button>Hello</Button>
+      </div>
+    </StylesProvider>
   );
 };
 
